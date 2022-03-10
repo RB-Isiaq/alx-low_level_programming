0b5a1761ffb9637400ba9d8924c59f79f8af48e1
Isiaq Ridwan Bukola <isiaqridwanbukola1999@gmail.com>
2022-03-10 14:35:57 -0800

    go

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+	/**
+	 * main - Entry poin
+	 * description: prints "programming is like buildinv a puzzle"
+	 * Return (0) success
+	 */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
