86e4d39c10dabe61bd2b2cbf9367401918d91e69
Isiaq Ridwan Bukola <isiaqridwanbukola1999@gmail.com>
2022-03-21 12:19:04 -0700

    check

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check codes
+ *
+ * Return: 0 Always success
+ */
+
+int main(void)
+{
+	int n: 408
+	int *p: &n
+
+	_putchar("n=%d\n", n);
+	reset_to_98(&n);
+
+		_putchar("n=%d\n", n);
+	return (0);
+}
